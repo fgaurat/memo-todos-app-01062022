08e5b09680ec572b98332500637072a753413314
Frédéric Gaurat <fgaurat@eolem.com>
2022-06-01 11:50:37 +0200

    first

---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function TodoList() {
+  return (
+    <>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>title</th>
+                    <th>completed</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+
+            </tbody>
+        </table>
+    </>
+  )
+}
+
+export default TodoList
